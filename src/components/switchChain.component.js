b36ca0a4e280fc0f5bd546c9be51314f0cb3757a
Jonah Madeya <jonah@thecoolagency.com>
2022-02-02 06:48:52 -0800

    fix js operators in switch chain components

--- src/components/switchChain.component.js
+++ src/components/switchChain.component.js
@@ -7,23 +7,23 @@ function SwitchChain() {
 
     <div>
 
-      {chainId == "0x1" &&
+      {chainId === "0x1" &&
         <p>Current chainId: {chainId} - Ethereum Mainnet</p>
       }
-      {chainId == "0x38" &&
+      {chainId === "0x38" &&
         <p>Current chainId: {chainId} - Binance Smart Chain Mainnet</p>
       }
-      {chainId == "0x89" &&
+      {chainId === "0x89" &&
         <p>Current chainId: {chainId} - Polygon Mainnet</p>
       }
 
-      {chainId != "0x1" &&
+      {chainId !== "0x1" &&
         <button onClick={() => switchNetwork("0x1")}>Switch to Ethereum Mainnet</button>
       }
-      {chainId != "0x38" &&
+      {chainId !== "0x38" &&
       <button onClick={() => switchNetwork("0x38")}>Switch to Binance Smart Chain Mainnet</button>
       }
-      {chainId != "0x89" &&
+      {chainId !== "0x89" &&
       <button onClick={() => switchNetwork("0x89")}>Switch to Polygon Mainnet</button>
       }
       
