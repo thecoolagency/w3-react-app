42ef168ffd418bd2409bdfa15d10cb240034e5f2
Jonah Madeya <jonah@thecoolagency.com>
2022-02-02 08:06:48 -0800

    update component logic

--- src/components/ERC20Balances.component.js
+++ src/components/ERC20Balances.component.js
@@ -1,28 +1,45 @@
 import { useERC20Balances } from "react-moralis";
 
 function ERC20Balances(props) {
-  const { data: assets } = useERC20Balances(props);
+  const { data: assets, isLoading, isFetching, error } = useERC20Balances(props);
 
   console.log(assets);
 
 if (!assets) return null;
 
-  return (
+  if (isLoading)
+    return (
+      <div>IS LOADING ...</div>
+    );
 
-    <div className="list list-tokens">
-      <h5>Total tokens: {assets.length}</h5>
-      <ul>
-        {assets.map(asset => (
-          <li key={asset}>
-            <span>{asset.name}</span> |&nbsp;
-            <span>{asset.symbol}</span> |&nbsp;
-            <span>{asset.balance}</span> |&nbsp;
-            <span>{asset.token_address}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
-    
-  );
+  if (isFetching)
+    return (
+      <div>IS FETCHING ...</div>
+    );
+
+  else
+  
+    return (
+
+      <div className="list list-tokens">
+        <h5>Total tokens: {assets.length}</h5>
+        <ul>
+          {assets.map(asset => (
+            <li key={asset}>
+              <span>{asset.name}</span> |&nbsp;
+              <span>{asset.symbol}</span> |&nbsp;
+              <span>{asset.balance}</span> |&nbsp;
+              <span>{asset.token_address}</span>
+            </li>
+          ))}
+          {error && <>{JSON.stringify(error)}</>}
+          <code>
+            <pre>{JSON.stringify(assets, null, 2)}</pre>
+          </code>
+        </ul>
+      </div>
+      
+    );
+
 }
 export default ERC20Balances;
